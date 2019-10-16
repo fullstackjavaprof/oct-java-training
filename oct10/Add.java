1ed9147642608da9a973a45c82dc492645b1456f
fullstack java <45912929+fullstackjavaprof@users.noreply.github.com>
2019-10-16 07:30:38 +0530

    Add files via upload

---
+++ oct10/Add.java
@@ -0,0 +1,13 @@
+
+public class Add {
+	public static void main(String[] args) {
+        int a=5;
+        int b=2;
+       if(a<b) {
+    	   System.out.println("Big number "+b);
+       	}
+       else {
+    	   System.out.println("Big number "+a);
+       }
+	}
+}
